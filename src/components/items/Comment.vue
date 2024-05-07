758e51b46db69c2a0420246c7cfb4db5807b6a35
Killer66562 <jimmy688382@gmail.com>
2024-05-07 18:33:18 +0800

    Add user.png

--- src/components/items/Comment.vue
+++ src/components/items/Comment.vue
@@ -11,7 +11,7 @@ defineProps<{
         <div class="col-3 col-sm-10 col-md-1">
             <RouterLink :to="`/users/${comment?.user_id}`">
                 <img class="img-fluid rounded-circle" :src="getStaticFile(comment.user.icon)" style="aspect-ratio: 1 / 1;"  v-if="comment.user.icon">
-                <img class="img-fluid rounded-circle" src="/src/assets/store.png" style="aspect-ratio: 1 / 1;" v-else>
+                <img class="img-fluid rounded-circle" src="/src/assets/user.png" style="aspect-ratio: 1 / 1;" v-else>
             </RouterLink>
         </div>
         <div class="col-9 col-sm-10 col-md-11">
